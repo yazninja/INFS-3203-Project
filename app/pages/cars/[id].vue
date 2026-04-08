570d22744fe66f47ecfc57f2039e92636c8e2a98
yazninja <yazlesean@gmail.com>
2026-04-08 14:41:14 +0300

    feat: update car card navigation to open in a new tab and add redirect page for car details

---
+++ app/pages/cars/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Redirecting you to Qatar Living...
+  </div>
+</template>
+
+<script lang="ts" setup>
+const $route = useRoute();
+navigateTo(`https://www.qatarliving.com/en/vehicles/cars/${$route.params.id}`, {
+  external: true
+});
+</script>
+
+<style></style>
